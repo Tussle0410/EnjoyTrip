16d233fd0406ee35414724c2d7376c6f677c0413
이찬형 <cksgud410@gmail.com>
2023-03-24 16:23:05 +0900

    dtoUpdate

--- src/com/ssafy/review/model/ReviewDto.java
+++ src/com/ssafy/review/model/ReviewDto.java
@@ -1,5 +1,46 @@
 package com.ssafy.review.model;
 
-public class ReviewDto {
+import java.sql.Date;
 
+public class ReviewDto {
+	private int reviewNo;
+	private int articleNo;
+	private String content;
+	private Date registTime;
+	private String email;
+	public int getReviewNo() {
+		return reviewNo;
+	}
+	public void setReviewNo(int reviewNo) {
+		this.reviewNo = reviewNo;
+	}
+	public int getArticleNo() {
+		return articleNo;
+	}
+	public void setArticleNo(int articleNo) {
+		this.articleNo = articleNo;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegistTime() {
+		return registTime;
+	}
+	public void setRegistTime(Date registTime) {
+		this.registTime = registTime;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "ReviewDto [reviewNo=" + reviewNo + ", articleNo=" + articleNo + ", content=" + content + ", registTime="
+				+ registTime + ", email=" + email + "]";
+	}
 }
