027550ca6f759c6992c570076f811cd8dafdaa97
이찬형 <cksgud410@gmail.com>
2023-03-27 15:22:50 +0900

    update

--- src/com/ssafy/attraction/controller/AttractionController.java
+++ src/com/ssafy/attraction/controller/AttractionController.java
@@ -1,13 +1,16 @@
 package com.ssafy.attraction.controller;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ssafy.attraction.model.dto.SidoDto;
 import com.ssafy.attraction.model.service.AttractionService;
 import com.ssafy.attraction.model.service.AttractionServiceImpl;
 
@@ -20,16 +23,42 @@ public class AttractionController extends HttpServlet{
 	@Override
 	public void init() throws ServletException {
 		super.init();
+		attractionService = AttractionServiceImpl.getInstance();
 	}
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doGet(req, resp);
+		String action = req.getParameter("action");
+		String path = "";
+		if("sidoFind".equals(action)) {
+			path = sidoFind(req, resp);
+			forward(req, resp, path);
+		}else if("gugunFind".equals(action)) {
+			
+		}
 	}
 	
 	
+	private String sidoFind(HttpServletRequest req, HttpServletResponse resp) {
+		try {
+			List<SidoDto> sidos = attractionService.sidoFindByAll();
+			req.setAttribute("sidos", sidos);
+		}catch(Exception e) {
+			e.printStackTrace();
+			return "/view/error/error.jsp";
+		}
+	}
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doPost(req, resp);
+		req.setCharacterEncoding("utf-8");
+		doGet(req, resp);
+	}
+	private void forward(HttpServletRequest req, HttpServletResponse resp, String path) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher(path);
+		dispatcher.forward(req, resp);
+	}
+	private void redirect(HttpServletRequest req, HttpServletResponse resp, String path) throws IOException {
+		resp.sendRedirect(req.getContextPath() + path);
 	}
 }
--- src/com/ssafy/attraction/model/service/AttractionServiceImpl.java
+++ src/com/ssafy/attraction/model/service/AttractionServiceImpl.java
@@ -1,5 +1,41 @@
 package com.ssafy.attraction.model.service;
 
+
+import java.util.List;
+
+import com.ssafy.attraction.model.dao.AttractionDao;
+import com.ssafy.attraction.model.dao.AttractionDaoImpl;
+import com.ssafy.attraction.model.dto.GugunDto;
+import com.ssafy.attraction.model.dto.SidoDto;
+import com.ssafy.board.model.dao.ArticleDao;
+import com.ssafy.board.model.dao.ArticleDaoImpl;
+import com.ssafy.util.DBUtil;
+
 public class AttractionServiceImpl implements AttractionService{
+	
+	private AttractionDao attractionDao;
+	private static AttractionService instance;
+	
+	private AttractionServiceImpl() {
+		attractionDao = AttractionDaoImpl.getInstance();
+	}
+	
+	public static AttractionService getInstance() {
+		if(instance == null)
+			return new AttractionServiceImpl();
+		else
+			return instance;
+	}
+	
+	
+	@Override
+	public List<SidoDto> sidoFindByAll() throws Exception {
+		return attractionDao.sidoFindByAll();
+	}
+
+	@Override
+	public List<GugunDto> gugunFindBySido(SidoDto sidoDto) throws Exception {
+		return attractionDao.gugunFindBySido(sidoDto);
+	}
 
 }
